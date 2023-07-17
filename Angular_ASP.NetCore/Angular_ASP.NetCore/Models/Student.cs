3439db1d5a3b8dc998a3aa157002695b8d005f68
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-07-17 22:56:57 +0300

    Angular_NetCore
    Before migrations

---
+++ Angular_ASP.NetCore/Angular_ASP.NetCore/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Angular_ASP.NetCore.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Course { get; set; }
+    }
+}
---
+++ Angular_ASP.NetCore/Angular_ASP.NetCore/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Angular_ASP.NetCore.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog=STUDENT; Integrated Security=True; TrustServerCertificate=True;");
+        }
+    }
+}
